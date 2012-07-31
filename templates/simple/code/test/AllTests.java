4fe4bece20b2f551a6b741508df257583ef0f978
arad0726 <arad0726@26cd2a52-c9c5-0410-8157-a43b28e47565>
2012-07-31 12:19:37 +0000

    git-svn-id: http://svn.it.usyd.edu.au/svn/infosubsys@11 26cd2a52-c9c5-0410-8157-a43b28e47565

---
+++ templates/simple/code/test/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ task1.SimpleCalcTest.class })
+public class AllTests {
+
+}
