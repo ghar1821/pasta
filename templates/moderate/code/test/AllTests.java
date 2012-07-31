2a87dcaea10ad8cf63772471aac0238b31f058ba
arad0726 <arad0726@26cd2a52-c9c5-0410-8157-a43b28e47565>
2012-07-31 12:20:30 +0000

    git-svn-id: http://svn.it.usyd.edu.au/svn/infosubsys@12 26cd2a52-c9c5-0410-8157-a43b28e47565

---
+++ templates/moderate/code/test/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ task4.SplitterTest.class })
+public class AllTests {
+
+}
---
+++ templates/moderate/code/test/security/CustomSecurityManager.java
@@ -0,0 +1,63 @@
+package security;
+
+import java.security.Permission;
+import java.util.PropertyPermission;
+
+public class CustomSecurityManager extends SecurityManager{
+	// block EVERYTHING
+	public CustomSecurityManager(){
+		System.out.println("SecurityManager replaced");
+	}
+	
+	@Override
+	public void checkPermission(Permission perm) {
+
+		if(perm.implies(new RuntimePermission("setIO")) || 
+				perm.implies(new PropertyPermission("user.dir", "read")) ||
+				perm.implies(new PropertyPermission("line.separator", "read")) ||
+				perm.implies(new RuntimePermission("setContextClassLoader"))||
+				perm.implies(new RuntimePermission("accessDeclaredMembers"))||
+				perm.implies(new RuntimePermission("getProtectionDomain"))){
+				// do nothing
+			return;
+		}
+		super.checkPermission(perm);
+	}
+	
+	@Override
+	public void checkExit(int status) {
+		// do nothing
+	}
+	
+	@Override
+	public void checkRead(String file) {
+		// TODO Auto-generated method stub
+		if(file.matches(".*/other/ant/.*/lib/.*\\.jar") ||
+				file.matches(System.getProperty("user.dir")+".*") ||
+				file.matches("test/input[1234567].txt")){
+			// do nothing
+			return;
+		}
+		super.checkRead(file);
+	}
+	
+	@Override
+	public void checkDelete(String file) {
+		if(file.matches(System.getProperty("user.dir")+"/bin.*")){
+			// do nothing
+			return;
+		}
+		super.checkDelete(file);
+	}
+	
+	@Override
+	public void checkWrite(String file) {
+		if(file.endsWith("TEST-AllTests.xml") ||
+			file.matches("test/input[1234567]-numbers.txt") ||
+			file.matches("test/input[1234567]-words.txt")){
+			// do nothing
+			return;
+		}
+		super.checkWrite(file);
+	}	
+}
