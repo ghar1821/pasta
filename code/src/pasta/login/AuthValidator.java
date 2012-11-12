f12b87efe985aa6c07d41066b8af938a79d90f6d
arad0726 <arad0726@26cd2a52-c9c5-0410-8157-a43b28e47565>
2012-11-12 23:41:28 +0000

    disabled authentication system for testing.
    git-svn-id: http://svn.it.usyd.edu.au/svn/infosubsys@21 26cd2a52-c9c5-0410-8157-a43b28e47565

--- code/src/pasta/login/AuthValidator.java
+++ code/src/pasta/login/AuthValidator.java
@@ -38,55 +38,55 @@ public class AuthValidator implements Validator {
 	 */
 	public void validate(Object target, Errors errors) {
 		LoginForm login = (LoginForm) target;
-		// reject if the username is empty
-		if (login.getUnikey() == null || login.getUnikey().length() == 0) {
-			errors.rejectValue("unikey", "NotEmpty.loginForm.unikey");
-		}
-		// reject if the password is empty
-		if (login.getPassword() == null || login.getPassword().length() == 0) {
-			errors.rejectValue("password", "NotEmpty.loginForm.password");
-		}
-		
-		// if nothing has gone wrong yet, check using the mail servers. 
-		if (!errors.hasErrors()){
-			String unikey = login.getUnikey();
-			String password = login.getPassword();
-			// Initialise mail session and check username and password.
-			try{
-				Properties props = new Properties();
-				props.setProperty("mail.imap.socketFactory.fallback", "false");
-				props.setProperty("mail.imap.port", "143");
-				props.setProperty("mail.imap.socketFactory.port", "143");
-				props.put("mail.imap.auth.plain.disable","true"); 
-	 
-				Session session = null;			
-				session = Session.getInstance(props);
-				session.setDebug(false);			
-				javax.mail.Store store = session.getStore(new 
-			               javax.mail.URLName("imap://imap.it.usyd.edu.au"));
-				store.connect("imap.it.usyd.edu.au",143,unikey,password);
-			}catch (Exception e) {
-				System.out.println("Didn't auth against normal server, trying ug");
-				try{
-					Properties props = new Properties();
-					props.setProperty("mail.imap.socketFactory.fallback", "false");
-					props.setProperty("mail.imap.port", "143");
-					props.setProperty("mail.imap.socketFactory.port", "143");
-					props.put("mail.imap.auth.plain.disable","true"); 
-		 
-					Session session = null;			
-					session = Session.getInstance(props);
-					session.setDebug(false);			
-					javax.mail.Store store = session.getStore(new 
-				               javax.mail.URLName("imap://imap.ug.cs.usyd.edu.au"));
-					store.connect("imap.ug.cs.usyd.edu.au",143,unikey,password);
-					
-				}
-				catch (Exception ex){
-					errors.rejectValue("password", "Failed.loginForm.password");
-				}
-			}
-		}
+//		// reject if the username is empty
+//		if (login.getUnikey() == null || login.getUnikey().length() == 0) {
+//			errors.rejectValue("unikey", "NotEmpty.loginForm.unikey");
+//		}
+//		// reject if the password is empty
+//		if (login.getPassword() == null || login.getPassword().length() == 0) {
+//			errors.rejectValue("password", "NotEmpty.loginForm.password");
+//		}
+//		
+//		// if nothing has gone wrong yet, check using the mail servers. 
+//		if (!errors.hasErrors()){
+//			String unikey = login.getUnikey();
+//			String password = login.getPassword();
+//			// Initialise mail session and check username and password.
+//			try{
+//				Properties props = new Properties();
+//				props.setProperty("mail.imap.socketFactory.fallback", "false");
+//				props.setProperty("mail.imap.port", "143");
+//				props.setProperty("mail.imap.socketFactory.port", "143");
+//				props.put("mail.imap.auth.plain.disable","true"); 
+//	 
+//				Session session = null;			
+//				session = Session.getInstance(props);
+//				session.setDebug(false);			
+//				javax.mail.Store store = session.getStore(new 
+//			               javax.mail.URLName("imap://imap.it.usyd.edu.au"));
+//				store.connect("imap.it.usyd.edu.au",143,unikey,password);
+//			}catch (Exception e) {
+//				System.out.println("Didn't auth against normal server, trying ug");
+//				try{
+//					Properties props = new Properties();
+//					props.setProperty("mail.imap.socketFactory.fallback", "false");
+//					props.setProperty("mail.imap.port", "143");
+//					props.setProperty("mail.imap.socketFactory.port", "143");
+//					props.put("mail.imap.auth.plain.disable","true"); 
+//		 
+//					Session session = null;			
+//					session = Session.getInstance(props);
+//					session.setDebug(false);			
+//					javax.mail.Store store = session.getStore(new 
+//				               javax.mail.URLName("imap://imap.ug.cs.usyd.edu.au"));
+//					store.connect("imap.ug.cs.usyd.edu.au",143,unikey,password);
+//					
+//				}
+//				catch (Exception ex){
+//					errors.rejectValue("password", "Failed.loginForm.password");
+//				}
+//			}
+//		}
 
 		// if the login is successful
 		if (!errors.hasErrors()) {
