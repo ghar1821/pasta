6e5120bcad8d62c4754f2f9f9fef456e60bbcc45
arad0726 <arad0726@26cd2a52-c9c5-0410-8157-a43b28e47565>
2012-11-13 08:41:38 +0000

    Started on #44.
    Added TODO tags for #47, #48 and #49
    
    git-svn-id: http://svn.it.usyd.edu.au/svn/infosubsys@28 26cd2a52-c9c5-0410-8157-a43b28e47565

--- code/src/pasta/repository/AssessmentDAO.java
+++ code/src/pasta/repository/AssessmentDAO.java
@@ -1,4 +1,40 @@
 package pasta.repository;
 
+import java.util.Collection;
+import java.util.HashMap;
+
+import pasta.domain.template.Assessment;
+import pasta.domain.template.Competition;
+import pasta.domain.template.HandMarking;
+import pasta.domain.template.UnitTest;
+
 public class AssessmentDAO {
+	
+	// assessmentTemplates are cached
+	HashMap<String, Assessment> allAssessments;
+	HashMap<String, UnitTest> allUnitTests;
+	HashMap<String, HandMarking> allHandMarking;
+	HashMap<String, Competition> allCompetitions;
+	
+	public AssessmentDAO(){
+		// load up all cached objects
+		
+		// load up unit tests TODO #44
+		
+		// load up hand marking TODO #47
+		
+		// load up competitions TODO #48
+		
+		// load up all assessments TODO #49
+
+	}
+	
+	public Assessment getAssessment(String name){
+		return allAssessments.get(name);
+	}
+	
+	public Collection getAssessmentList(){
+		return allAssessments.values();
+	}
+	
 }
