13aa72dae67145841ff00318afe90fe734cfdd83
Joshua Stretton <joshua.stretton@sydney.edu.au>
2015-08-27 22:31:39 +1000

    Fixed variable typos in sample validator help page.

--- war/WEB-INF/template_content/help_templates/SampleCustomValidator.java
+++ war/WEB-INF/template_content/help_templates/SampleCustomValidator.java
@@ -11,9 +11,9 @@ public class MyValidator implements PASTASubmissionValidator {
 	// This feedback is given to all users, and does not prevent the submission from being accepted
 	public Collection<ValidationFeedback> getFeedback(String username, File submissionBase) {
 		Collection<ValidationFeedback> feedback = new LinkedList<>();
-		list.add(new ValidationFeedback("Your username is " + username));
+		feedback.add(new ValidationFeedback("Your username is " + username));
 		if(new File(submissionBase, "extra_work.txt").exists()) {
-			list.add(new ValidationFeedback("Good Work!",
+			feedback.add(new ValidationFeedback("Good Work!",
 					"It looks like you've done the extra work."));
 		}
 		return feedback;
@@ -23,9 +23,9 @@ public class MyValidator implements PASTASubmissionValidator {
 	public Collection<ValidationFeedback> getErrors(String username, File submissionBase) {
 		Collection<ValidationFeedback> errors = new LinkedList<>();
 		if(!new File(submissionBase, "expected_work.txt").exists()) {
-			list.add(new ValidationFeedback("Files Expected",
+			errors.add(new ValidationFeedback("Files Expected",
 					"You haven't included expected_work.txt in your submission."));
 		}
-		return list;
+		return errors;
 	}
 }
