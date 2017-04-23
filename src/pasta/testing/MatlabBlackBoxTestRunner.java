bc2ddc0c86d1d369d52f239668e5065ac81351d4
Joshua Stretton <joshua.stretton@sydney.edu.au>
2017-04-23 19:24:41 +1000

    Updated MATLAB unit tests to run by compiling their code with mcc before running a stand-alone program for each black box test.

--- src/pasta/testing/MatlabBlackBoxTestRunner.java
+++ src/pasta/testing/MatlabBlackBoxTestRunner.java
@@ -1,6 +1,7 @@
 package pasta.testing;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import pasta.util.PASTAUtil;
 
@@ -15,5 +16,36 @@ public class MatlabBlackBoxTestRunner extends BlackBoxTestRunner {
 	
 	public MatlabBlackBoxTestRunner() throws FileNotFoundException {
 		super(PASTAUtil.getTemplateResource("build_templates/" + TEMPLATE_FILENAME));
+	}
+	
+	@Override
+	public String extractCompileErrors(AntResults results) {
+		Scanner scn = new Scanner(results.getOutput("build"));
+		StringBuilder compErrors = new StringBuilder();
+		String line = "";
+		boolean appending = false;
+		if(scn.hasNext()) {
+			while(scn.hasNextLine()) {
+				line = scn.nextLine().trim();
+				if(line.startsWith("[exec] ")) {
+					line = line.substring(7);
+				} else {
+					continue;
+				}
+				if(!appending && line.toLowerCase().replaceAll("[^a-z]", "").startsWith("error")) {
+					appending = true;
+				}
+				if(appending && line.toLowerCase().contains("error in")) {
+					appending = false;
+				}
+				if(appending) {
+					compErrors.append(line).append('\n');
+				}
+			}
+			// Chop off the trailing "\n"
+			compErrors.deleteCharAt(compErrors.length()-1);
+		}
+		scn.close();
+		return compErrors.toString();
 	}
 }
