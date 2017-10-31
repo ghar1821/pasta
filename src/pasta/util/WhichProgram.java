7af7fe082b4e7468fb88a4606fa4e1b767305f30
Joshua Stretton <joshua.stretton@sydney.edu.au>
2017-10-31 19:22:57 +1100

    Options page no longer needs to be able to change program paths due to docker install.

--- src/pasta/util/WhichProgram.java
+++ src/pasta/util/WhichProgram.java
@@ -1,14 +1,17 @@
 package pasta.util;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
-import pasta.service.PASTAOptions;
-
 public class WhichProgram {
 
+	private Map<String, String> programs;
+	
 	private static WhichProgram instance;
 	
 	protected final static Logger logger = Logger.getLogger(WhichProgram.class);
@@ -16,7 +19,16 @@ public class WhichProgram {
 	private static final String[] required = {"java", "javac", "time", "timeout", "mysqldump"};
 	private static final String[] optional = {"matlab", "python2", "python3", "g++", "gcc"};
 	
-	private WhichProgram() {
+	private WhichProgram(Properties properties) {
+		programs = new HashMap<String, String>();
+		
+		for(Object propKey : properties.keySet()) {
+			String key = (String) propKey;
+			String value = properties.getProperty(key);
+			programs.put(key, value);
+			logger.debug("Registering program " + key + " at " + value);
+		}
+		
 		List<String> missing = new LinkedList<>();
 		for(String program : required) {
 			if(!hasProgram(program)) {
@@ -24,7 +36,7 @@ public class WhichProgram {
 			}
 		}
 		if(missing.size() != 0) {
-			logger.error("Missing required program paths in options: " + missing.toString());
+			logger.error("Missing required program paths in programs.properties: " + missing.toString());
 		}
 		
 		missing = new LinkedList<>();
@@ -34,18 +46,18 @@ public class WhichProgram {
 			}
 		}
 		if(missing.size() != 0) {
-			logger.info("Missing optional program paths in options: " + missing.toString());
+			logger.info("Missing optional program paths in programs.properties: " + missing.toString());
 		}
 		
 		WhichProgram.instance = this;
 	}
 	
 	public boolean hasProgram(String program) {
-		return PASTAOptions.instance().hasKey("programs." + program);
+		return programs.containsKey(program) && programs.get(program) != null;
 	}
 	
 	public String path(String program) {
-		return PASTAOptions.instance().get("programs." + program);
+		return programs.get(program);
 	}
 	
 	public static WhichProgram getInstance() {
